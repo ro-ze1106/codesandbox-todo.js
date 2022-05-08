25b9ef4fccaa3fdf7a5cb6091d7942d1ead993d6
ro-ze1106 <85288297+ro-ze1106@users.noreply.github.com>
2022-05-08 16:44:07 +0900

    タスク追加機能（ボタン）

--- src/index.js
+++ src/index.js
@@ -5,16 +5,32 @@ const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
-  //　 li生成
-  const li = document.createElement("li");
-  li.className = "list-row";
-
-  // div生成
+  //　 div生成
   const div = document.createElement("div");
-  div.innerText = inputText;
+  div.className = "list-row";
 
-  // liタグの子要素に各要素を設定
-  li.appendChild(div);
+  // li生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // button（完了）タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  // button（削除）タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+
+  // divタグの子要素に各要素を設定
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   // 未完成リストに追加
   document.getElementById("incomplete-list").appendChild(div);
