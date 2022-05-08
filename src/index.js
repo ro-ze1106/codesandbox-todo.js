df153c9809cf1bfaae75f748a6f08a139b7f3c90
ro-ze1106 <85288297+ro-ze1106@users.noreply.github.com>
2022-05-08 15:57:04 +0900

    タスク追加機能（テキスト）

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //　 li生成
+  const li = document.createElement("li");
+  li.className = "list-row";
+
+  // div生成
+  const div = document.createElement("div");
+  div.innerText = inputText;
+
+  // liタグの子要素に各要素を設定
+  li.appendChild(div);
+
+  // 未完成リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
